c4d8a1ec7e9693e39f4d4ff90b79cfcdb673eb21
stericardo <ricardostev@gmail.com>
2026-07-06 22:59:17 -0600

    RUST VS JAVA

--- postgresql/src/main.rs
+++ postgresql/src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     let start = Instant::now();
 	
 	//method call
-	call_postresql();	
+	//call_postresql();	
+	insert3();
 	
 	// 3. Calculate the elapsed duration
     let duration = start.elapsed();
@@ -32,10 +33,100 @@ fn call_postresql() -> Result<(), postgres::Error> {
         let email: &str = row.get(1);
         let first_name: &str = row.get(2);
         let last_name: &str = row.get(3);
-        println!("Found user: {} with id: {}, email:{}, last_name:{}", first_name, id, email, last_name);
+		println!("Found user: {} with id: {}, email:{}, last_name:{}", first_name, id, email, last_name);
     }
+    
 
     Ok(())
 }
 
+fn insert() -> Result<(), postgres::Error>  {
+	
+	let mut client = Client::connect(
+        "postgresql://postgres:2rHPwHr0pu2k@localhost:5432/postgres", 
+        NoTls
+    )?;
+	    for i in 1..=1000000 {
+        // 2. Define the record variables
+        let first_name = format!("User {}", i);
+		let email = format!("ricardostev{}@gmail.com", i);
+		let last_name = "Mendez";
+		
+		match client.execute(
+			"INSERT INTO users (email, first_name, last_name) VALUES ($1, $2, $3)",
+			&[&email, &first_name, &last_name],
+		){
+			Ok(rows_affected) => println!("Inserted {} row(s).", rows_affected),
+			Err(e) => {
+				// Print the high-level error message
+				eprintln!("Execution error: {}", e);
+				
+				// Print the underlying database details (if available)
+				if let Some(db_error) = e.as_db_error() {
+					eprintln!("DB Message: {}", db_error.message());
+					eprintln!("DB Code: {:?}", db_error.code());
+					}
+				}
+			}
+    }
+    
+    Ok(())
 
+	
+}
+
+
+fn insert2() -> Result<(), postgres::Error>  {
+	
+	let mut client = Client::connect(
+        "postgresql://postgres:2rHPwHr0pu2k@localhost:5432/postgres", 
+        NoTls
+    )?;
+	    for i in 1000001..=2000000 {
+        // 2. Define the record variables
+        let first_name = format!("User {}", i);
+		let email = format!("ricardostev{}@gmail.com", i);
+		let last_name = "Mendez";
+		
+		client.execute(
+			"INSERT INTO users (email, first_name, last_name) VALUES ($1, $2, $3)",
+			&[&email, &first_name, &last_name],
+		)?;
+    }
+    
+    println!("Record inserted successfully!");
+    Ok(())
+
+	
+}
+
+fn insert3() -> Result<(), postgres::Error>  {
+	
+	let mut client = Client::connect(
+        "postgresql://postgres:2rHPwHr0pu2k@localhost:5432/postgres", 
+        NoTls
+    )?;
+    
+    // 3. Start a transaction for synchronous/batched insertion
+    let mut transaction = client.transaction()?;
+    
+	    for i in 2000001..=2100000 {
+        // 2. Define the record variables
+        let first_name = format!("User {}", i);
+		let email = format!("ricardostev{}@gmail.com", i);
+		let last_name = "Mendez";
+		
+		transaction.execute(
+			"INSERT INTO users (email, first_name, last_name) VALUES ($1, $2, $3)",
+			&[&email, &first_name, &last_name],
+		)?;
+    }
+    
+    // 4. Commit the transaction
+    transaction.commit()?;
+    
+    println!("Record inserted successfully Transaction!");
+    Ok(())
+
+	
+}
